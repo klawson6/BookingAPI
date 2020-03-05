49dc3ef2a377204d90ece2e8f7b50b4aa94749dd
Kyle Lawson <kylelawson6@yahoo.com>
2020-03-05 00:19:54 +0000

    Cleaned up unused imports. Plan to try hosting on azure server.

--- Models/BookingDBContext.cs
+++ Models/BookingDBContext.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Web;
 
 namespace BookingAPI.Models {
     public class BookingDBContext : DbContext {
--- Models/Hotel.cs
+++ Models/Hotel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-
-namespace BookingAPI.Models {
+﻿namespace BookingAPI.Models {
     public class Hotel {
         public long Id { get; set; }
         public string name { get; set; }
